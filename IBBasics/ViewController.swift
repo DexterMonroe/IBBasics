d54a0be0bfd86e323850c9db97935cf8d956698d
Dexter Monroe <admin@dextermonroe.com>
2023-07-24 12:07:35 -0500

    Initial Commit

---
+++ IBBasics/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  IBBasics
+//
+//  Created by admin on 7/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+// MARK: - IB Outlets
+    
+    
+    
+    
+    
+    // MARK: - Instance Properties
+    
+    
+    
+    //MARK: - VDL
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+// MARK: - IB Actions
+    
+    
+
+    
+// MARK: - Instance Methods
+    
+    
+    
+}
+
